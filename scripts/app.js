d8fc60da74f017a967e7c67165107305dc10e33d
yousifj129 <yousifjaffar129@gmail.com>
2025-07-09 13:32:27 +0300

    added accuracy and difficulty selector

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,79 @@
-function init(){
-    const gridWrapperElem = document.querySelector(".grid-wrapper")
+function init() {
     const gridElem = document.querySelector(".grid")
-    const textElem = document.querySelector("h1")
+    const textElem = document.querySelector("#text")
+    const restartElem = document.querySelector("#restart")
+    const difficultySelectorElem = document.querySelector("#difficultySelector")
     const cells = []
+    difficulties = [
+        2000,
+        1000,
+        800,
+        500,
+        300
+    ]
+    let timerOfAppear = 800;
     let score = 0
     let currentCell;
     let intrevalId;
-    function createGrid(){
-        
-        for(let i=0; i<100; i++){
+    let userClicks = 0;
+    let gameRunning = false;
+    function createGrid() {
+        for (let i = 0; i < 100; i++) {
             const cell = document.createElement("div")
-            
+
             cell.className = "cell"
             cells.push(cell)
             gridElem.append(cell)
         }
     }
-    function play(){
-        if(currentCell != undefined){
+    function play() {
+        timerOfAppear = difficulties[Number(difficultySelectorElem.value)]
+        if (currentCell != undefined) {
             currentCell.remove()
         }
-        if(score > 900)
-        {
+        if (score > 900) {
             textElem.textContent = "YOU WON!"
             clearInterval(intrevalId)
-            
+
         }
-        else{
-            const chosenCell = cells[Math.floor(Math.random()*100)]
-            console.log(chosenCell)
+        else {
+            const chosenCell = cells[Math.floor(Math.random() * 100)]
             const duck = document.createElement("div")
             duck.className = "duck"
             chosenCell.appendChild(duck)
-            duck.addEventListener("click", function(){
+            duck.addEventListener("click", function () {
                 score += 100;
-                textElem.textContent = `current score: ${score}/1000`
+                textElem.textContent = `current score: ${score}/1000, accuracy: ${(score/100)}/${userClicks}`
                 this.remove()
+                gameRunning = true
             })
             currentCell = duck
         }
 
-        
+
+    }
+    function start() {
+        userClicks = 0
+        timerOfAppear = difficulties[Number(difficultySelectorElem.value)]
+        score = 0;
+        clearInterval(intrevalId)
+        intrevalId = setInterval(play, timerOfAppear)
+        textElem.textContent = "click on the duck to start"
+        gameRunning = false;
     }
     createGrid()
-    intrevalId = setInterval(play, 2000);
+    intrevalId = setInterval(play, timerOfAppear);
+    restartElem.addEventListener("click", start)
+    difficultySelectorElem.addEventListener("change",start)
+    window.addEventListener("click", ()=>{
+        if(gameRunning){
+        userClicks += 1;textElem.textContent = `current score: ${score}/1000, accuracy: ${(score/100)}/${userClicks}`;
+
+        }
+    
+    })
+    
+    
 }
 
 
